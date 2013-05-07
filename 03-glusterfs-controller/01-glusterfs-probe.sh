9dbb5f36e3af86a87c0d742009abf97af8f73992
jman883 <jman883@gmail.com>
2013-05-07 12:06:58 +0300

    Single Node Setup only knows compute1. Can be extenden later on if necessary

--- 03-glusterfs-controller/01-glusterfs-probe.sh
+++ 03-glusterfs-controller/01-glusterfs-probe.sh
@@ -17,6 +17,6 @@
 
 # Probe GlusterFS peer hosts
 gluster peer probe compute1
-gluster peer probe compute2
-gluster peer probe compute3
-gluster peer probe compute4
+#gluster peer probe compute2
+#gluster peer probe compute3
+#gluster peer probe compute4
