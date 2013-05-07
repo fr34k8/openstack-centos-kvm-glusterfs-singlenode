f974bac9704f8a75c0760cea1a33236434b6ee99
jman883 <jman883@gmail.com>
2013-05-07 14:30:42 +0300

    Update 36-heat-install.sh

--- 07-openstack-controller/36-heat-install.sh
+++ 07-openstack-controller/36-heat-install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 # Install the cloud orchestration component "Heat"
-yum install -y openstack-heat*
+# - Setup scripts not yet finished -
+# TODO: Extend setup with Infos from here:
+# http://docs.openstack.org/developer/heat/getting_started/on_ubuntu.html
+
+#yum install -y openstack-heat*
