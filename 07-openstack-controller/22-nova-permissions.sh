998282425d08f01e4b923be41c64bb16ce78aa30
jman883 <jman883@gmail.com>
2013-05-07 14:38:01 +0300

    Update 22-nova-permissions.sh

--- 07-openstack-controller/22-nova-permissions.sh
+++ 07-openstack-controller/22-nova-permissions.sh
@@ -14,10 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # Set restrictive permissions for the Nova config file
 chmod 640 /etc/nova/nova.conf
 
 # Set the ownership for the Nova related directories
 chown -R root:nova /etc/nova
 chown -R nova:nova /var/lib/nova
+chown -R nova:nova /var/cache/libvirt
+chown -R nova:nova /var/run/libvirt
+chown -R nova:nova /var/lib/libvirt
+
+# Make Qemu run under the nova user and group
+sed -i 's/#user = "root"/user = "nova"/g' /etc/libvirt/qemu.conf
+sed -i 's/#group = "root"/group = "nova"/g' /etc/libvirt/qemu.conf
