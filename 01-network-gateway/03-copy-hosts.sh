503e4738f7cd2cda49e475a17f9674c413809d33
jman883 <jman883@gmail.com>
2013-05-07 11:59:59 +0300

    No additional Hosts, no copying

--- 01-network-gateway/03-copy-hosts.sh
+++ 01-network-gateway/03-copy-hosts.sh
@@ -21,7 +21,7 @@ cp ../config/hosts /etc/
 # Copy the hosts file to the other nodes. For convenience, prior to
 # copying you may use the ssh-copy-id command for password-less SSH
 # access.
-scp ../config/hosts root@compute2:/etc/
-scp ../config/hosts root@compute3:/etc/
-scp ../config/hosts root@compute4:/etc/
-scp ../config/hosts root@controller:/etc/
+#scp ../config/hosts root@compute2:/etc/
+#scp ../config/hosts root@compute3:/etc/
+#scp ../config/hosts root@compute4:/etc/
+#scp ../config/hosts root@controller:/etc/
