addf8bbda891947ea93bb5bc09fe05283918caf3
jman883 <jman883@gmail.com>
2013-05-10 21:57:50 +0200

    EPEL ist still necessary, readded it

--- 06-openstack-all/01-epel-add-repo.sh
+++ 06-openstack-all/01-epel-add-repo.sh
@@ -16,6 +16,6 @@
 
 
 # Add the EPEL repo: http://fedoraproject.org/wiki/EPEL
-# yum install -y http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install -y http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install -y http://repos.fedorapeople.org/repos/openstack/openstack-grizzly/rdo-release-grizzly-1.noarch.rpm
 
